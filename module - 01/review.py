7c677088f5a5f49704aefa0d1fea130a635fe851
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-09-25 23:25:24 +0600

    Module One added in a folder

---
+++ module - 01/review.py
@@ -0,0 +1,37 @@
+# # print("Hellow Python")
+# # print("Hellow","Python")
+# # print("Hellow", "Python", sep="--------")
+# #  ctrl + /
+#
+# # Variable
+# name = "Ananta Jalil"
+# movie_name = "Code the Program"
+# print(name, movie_name, sep="*******")
+#
+# # Variable naming convention
+# # 1. Varibale name must be start with number or underscore.
+#
+# # Data Types
+# x = 1                   # Number
+# y = 'Abdul Aouwal'      # String - str
+# z = True                # Boolean - bool
+# a = ''
+#
+# p = 1                   # Integer - int
+# q = 2.5                 # Float - float
+#
+# # Type Function
+# print(type(x))
+# print(type(p))
+# print(type(y))
+# print(type(z))
+# print(type(q))
+#
+# # Data Casting
+# w = int(q)
+# print(w)
+
+length = 20
+width = 15
+area = length * width
+print('The Area of Rectangle is', area)
